c0f217aedfd5f01f1b27943f1e7eab9da12a8312
Jerome Schmidt <schmidt.jerome@yahoo.com>
2019-10-07 12:56:54 -0700

    started tests for Logger

---
+++ tests_logger.py
@@ -0,0 +1,16 @@
+from simulation import Simulation
+from virus import Virus
+from logger import Logger
+import unittest
+
+#UNIT TESTS
+class TestLogger(unittest.TestCase):
+
+    def test_write_metadata():
+        pass
+    def test_log_interaction():
+        pass
+    def test_log_infection_survival():
+        pass
+    def test_log_time_step():
+        pass
